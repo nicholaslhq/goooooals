ecb878898ae94b1a908cba5449b92f5842e15d0c
Nicholas Lee <niick0816@gmail.com>
2024-10-12 19:46:02 +0800

    Add Vercel Web Analytics and Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Provider from "./Provider";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -36,6 +38,8 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<Provider>{children}</Provider>
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
